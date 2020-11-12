44e80c0b2b120b45a1ec02a76f954c2415b90d54
dndnddndndnn <sungah1243@gmail.com>
2020-11-13 01:29:20 +0900

    gamestart

---
+++ Assets/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Class2 : MonoBehaviour
+{
+	public string SceneToLoad;
+	public void LoadGame()
+	{
+		SceneManager.LoadScene(SceneToLoad);
+	}
+}
